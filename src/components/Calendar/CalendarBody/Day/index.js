99e51a1977707a3ebdd8f95eba2044d94f8c5e23
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-12-02 00:15:33 +0200

    Render only current month days

--- src/components/Calendar/CalendarBody/Day/index.js
+++ src/components/Calendar/CalendarBody/Day/index.js
@@ -1,19 +1,23 @@
-import { format } from 'date-fns';
+import { format, getMonth } from 'date-fns';
 import React from 'react';
 import styles from './Day.module.css';
 import { withDay } from '../../../../HOCs/withDay';
 import cx from 'classnames';
 
 const Day = (props) => {
-    const {currentDay, day} = props;
+    const {currentDay, day, changeCurrentDay} = props;
     const cn = cx([styles.days],{
         [styles.current]: format(currentDay, 'd') === format(day, 'd')
     })
-    return (
-        <td className={cn}>
-            {format(day, 'd')}
-        </td>
-    );
+
+    if (getMonth(currentDay)===getMonth(day)){
+        return (
+            <td className={cn}>
+                {format(day, 'd')}
+            </td>
+        );
+    } else {return (<td></td>)}
+
 }
 
 const DayWithDay = withDay(Day);
